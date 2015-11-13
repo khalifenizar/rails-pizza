a498e66a4d559adaf9cc27138c44846b55da6d3a
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-11-13 17:32:59 -0500

    Add form submission action

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -4,4 +4,21 @@ class PizzasController < ApplicationController
 
     render :new
   end
+
+  def create
+    @pizza = Pizza.new(pizza_params)
+
+    if @pizza.save
+      redirect_to("/pizzas/#{@pizza.id}")
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def pizza_params
+    params.require(:pizza).permit(:crust, :sauce, :size)
+  end
 end
--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,6 @@
 class Pizza < ActiveRecord::Base
+  validates :crust, { presence: true }
+  validates :sauce, { presence: true }
+  validates :size, { presence: true }
+  validates :size, { inclusion: [ "small", "medium", "large" ] }
 end
